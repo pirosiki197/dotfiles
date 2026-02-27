d338c867e946d19fc0b741d078a67ce5b39ae35d
pirosiki197 <lingmukangtai825@gmail.com>
2026-02-28 02:17:41 +0900

    update treesitter

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,50 +1,59 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    lazy = false,
+    config = function()
+      require("nvim-treesitter").setup({
+        ensure_installed = {
+          "c",
+          "lua",
+          "vim",
+          "vimdoc",
+          "bash",
+          "go",
+          "gomod",
+          "dockerfile",
+          "json",
+          "typescript",
+          "rust",
+          "yaml",
+          "zig",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
   },
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "bash",
-        "go",
-        "gomod",
-        "dockerfile",
-        "json",
-        "typescript",
-        "rust",
-        "yaml",
-        "zig",
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+    end,
+    opts = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ir"] = "@assignment.rhs",
+        }
       },
-      highlight = { enable = true },
-      indent = { enable = true },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ir"] = "@assignment.rhs",
-          }
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          ["]a"] = "@parameter.inner",
         },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["]a"] = "@parameter.inner",
-          },
-          goto_previous_start = {
-            ["[a"] = "@parameter.inner",
-          },
+        goto_previous_start = {
+          ["[a"] = "@parameter.inner",
         },
       },
-    })
-  end,
+    },
+  },
 }
