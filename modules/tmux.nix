57e0f3480ef6ed99b24c852b479040eab2077025
pirosiki197 <lingmukangtai825@gmail.com>
2026-06-02 02:42:06 +0900

    update tmux.conf

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -7,10 +7,16 @@
     keyMode = "vi";
     escapeTime = 0;
     historyLimit = 10000;
+    terminal = "tmux-256color";
     plugins = with pkgs.tmuxPlugins; [
       sensible
       catppuccin
       yank
     ];
+    extraConfig = ''
+      set -ag terminal-overrides ",xterm-256color:RGB"
+      set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
+      set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
+    '';
   };
 }
