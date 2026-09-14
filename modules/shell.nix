814a916e5a6a3f77b1f58ee44065e10429ac5292
pirosiki197 <lingmukangtai825@gmail.com>
2026-09-14 12:23:50 +0900

    add zsh-fzf

--- modules/shell.nix
+++ modules/shell.nix
@@ -8,7 +8,7 @@
     enableCompletion = true;
     defaultKeymap = "emacs";
 
-    autosuggestion.enable = true;
+    autosuggestion.enable = false;
     syntaxHighlighting.enable = true;
 
     history = {
@@ -22,7 +22,22 @@
     };
 
     initContent = ''
+      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+      source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+      ZSH_AUTOSUGGEST_STRATEGY=(history)
+
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+      zstyle ':completion:*' menu no
+
+      zstyle ':fzf-tab:*' fzf-flags --height=60% --layout=reverse --border
+      zstyle ':fzf-tab:*' switch-group '<' '>'
+
+      zstyle ':fzf-tab:complete:cd:*' fzf-preview \
+      '${pkgs.eza}/bin/eza -1 --color=always $realpath'
+      zstyle ':fzf-tab:complete:*:*' fzf-preview \
+      '[[ -d $realpath ]] && ${pkgs.eza}/bin/eza -1 --color=always $realpath || ${pkgs.bat}/bin/bat --color=always $realpath 2>/dev/null || cat $realpath'
+      zstyle ':fzf-tab:complete:(-command-|-parameter-|export|unset):*' fzf-preview \
+      'echo ''${(P)word}'
 
       path=(
           $HOME/.local/bin
